23c98cac3c5f3729b1811ec97d9cee3af5550148
nzubemush <you@example.comnzubechukwu.okoye@gmail.com>
2019-11-03 16:16:07 +0100

    nav bar and colors fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <!-- Bootstrap core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
+    {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,55 +1,3 @@
-{{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'ValuationCalc') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="btn btn-outline-primary mx-2" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="btn btn-outline-primary mx-2" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
-    </div>
-</nav> --}}
-
-
 <div class="d-flex flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm sticky-top">
     {{-- <h5   href="{{ route('/') }}">{{ config('app.name', 'ValuationCalc') }}</h5> --}}
     <nav role='navigation' class="my-0 mr-auto font-weight-normal">
@@ -59,25 +7,49 @@
             <span></span>
             <span></span>
 
-            {{-- <ul id="menu">
-                @foreach ($categories as $category)
-                <a href="{{ route('category.show', ['categories' => $category]) }}"><li>{{ $category->name }}</li></a>
-                @endforeach
-            </ul> --}}
+            <ul id="menu">
+
+                <a href="/"><li>Home</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/4"><li>Actuarial Principles and Practice</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/5"><li>Economics</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/6"><li>Financial Management</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/7"><li>Financial Risk Management</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/8"><li>International Finance</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/9"><li>Investment Banking and Financial Services</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/10"><li>Management Accounting</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/11"><li>Portfolio Management</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/12"><li>Project Management</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/13"><li>Quantitative Methods</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/14"><li>Security Analysis</li></a>
+                <hr>
+                <a href="http://127.0.0.1:8000/categories/15"><li>Strategic Financial Management</li></a>
+
+            </ul>
         </div>
     </nav>
 
     <nav class="my-2 my-md-0">
-        <a class="p-2 text-dark" style="font-size: 13px" href="#">SUBSCRIBE</a>
-        <a class="p-2 text-dark" style="font-size: 13px" href="#">CONTACT</a>
+        <a class="p-2 text-dark navtext" style="font-size: 13px;" href="#">SUBSCRIBE</a>
+        <a class="p-2 text-dark navtext" style="font-size: 13px" href="#">CONTACT</a>
     </nav>
     @guest
 
-        <a class="btn btn-outline-primary mx-2" href="{{ route('login') }}">{{ __('Login') }}</a>
+        <a class="btn text-dark mx-2 navitext" href="{{ route('login') }}">{{ __('Login') }}</a>
 
         @if (Route::has('register'))
 
-            <a class="btn btn-outline-primary" href="{{ route('register') }}">{{ __('Register') }}</a>
+            <a class="btn text-dark navitext" href="{{ route('register') }}">{{ __('Register') }}</a>
 
         @endif
         @else
